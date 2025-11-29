105b6abba607057407417ad9dbccfee432957222
Alfredo <alf.alvarezr@duocuc.cl>
2025-11-29 17:14:00 -0300

    feat: Estructura archivos usuario y auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -20,6 +22,8 @@ import { AppService } from './app.service';
       entities: [],
       synchronize: true,
     }),
+    AuthModule,
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
